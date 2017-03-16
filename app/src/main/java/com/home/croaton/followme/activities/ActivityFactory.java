4f9b1252d93fd0672862d20972a3be1858d5137f
Lena Shervarly <lena.shervarly@gmail.com>
2017-03-16 17:29:18 +0100

    ajusting the switch of the  activity factory

--- app/src/main/java/com/home/croaton/followme/activities/ActivityFactory.java
+++ app/src/main/java/com/home/croaton/followme/activities/ActivityFactory.java
@@ -2,6 +2,7 @@ package com.home.croaton.followme.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 public class ActivityFactory {
     private int audioPointNumber;
@@ -12,26 +13,25 @@ public class ActivityFactory {
     }
 
     public Intent chooseActivity(Context context, int audioPointNumber){
-        String path = "android.activities://com.home.croaton.followme/activities/";
-        String nameOfChosenActivity;
+        //String nameOfChosenActivity;
 
         switch (audioPointNumber) {
-            case 0:  nameOfChosenActivity = "QuizzActivity";
+            case 0:  intent = new Intent(context, QuizzActivity.class);
                 break;
-            case 1:  nameOfChosenActivity = "QuizzActivity";
+            case 1:  intent = new Intent(context, QuizzActivity.class);
                 break;
-            default: nameOfChosenActivity = "QuizzActivity";
+            default: intent = new Intent(context, QuizzActivity.class);
                 break;
     }
-        Class<?> c = null;
+        /*Class<?> c = null;
         try {
-            c = Class.forName(path + nameOfChosenActivity);
+            Uri filePath = Uri.parse("android:com.home.croaton.followme/activities/" + nameOfChosenActivity);
+            c = Class.forName(filePath.toString());
             System.out.println(c.toString());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        }
+        }*/
 
-        intent = new Intent(context, c);
         return intent;
     }
 }
