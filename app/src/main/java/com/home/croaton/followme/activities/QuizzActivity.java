b3439641d6be1a1f9d4d3fa9ea228e6502d00b11
Temporary User 10231 <tmpuser-10231@kth.se>
2017-03-16 13:15:56 +0100

    created the Q a activity

--- app/src/main/java/com/home/croaton/followme/activities/QuizzActivity.java
+++ app/src/main/java/com/home/croaton/followme/activities/QuizzActivity.java
@@ -2,6 +2,9 @@ package com.home.croaton.followme.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.home.croaton.followme.R;
 
@@ -11,5 +14,39 @@ public class QuizzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.button1:
+                if (checked)
+
+                    Toast.makeText(this, "Right answer", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.button2:
+                if (checked)
+
+                    Toast.makeText(this, "Wrong answer", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.button3:
+                if (checked)
+
+                    Toast.makeText(this, "Wrong answer", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.button4:
+                if (checked)
+
+                    Toast.makeText(this, "Right answer", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+
     }
 }
