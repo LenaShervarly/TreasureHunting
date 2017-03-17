0d32c96943b92ec03b657a28f97100c2b9dd5dd6
kaankaraoglu <kaankaraoglu@outlook.com>
2017-03-17 13:36:12 +0100

    Added remainder to audioPointNumber check.

--- app/src/main/java/com/home/croaton/followme/activities/ActivityFactory.java
+++ app/src/main/java/com/home/croaton/followme/activities/ActivityFactory.java
@@ -13,25 +13,22 @@ public class ActivityFactory {
     }
 
     public Intent chooseActivity(Context context, int audioPointNumber){
-        //String nameOfChosenActivity;
+        //String nameOfChosenActivity; To be used later on in case of choosing activity by name.
 
-        switch (audioPointNumber) {
+        switch (audioPointNumber % 5) {
             case 0:  intent = new Intent(context, QuizzActivity.class);
                 break;
             case 1:  intent = new Intent(context, QuizzActivity.class);
                 break;
+            case 2:  intent = new Intent(context, QuizzActivity.class);
+                break;
+            case 3:  intent = new Intent(context, QuizzActivity.class);
+                break;
+            case 4:  intent = new Intent(context, QuizzActivity.class);
+                break;
             default: intent = new Intent(context, QuizzActivity.class);
                 break;
     }
-        /*Class<?> c = null;
-        try {
-            Uri filePath = Uri.parse("android:com.home.croaton.followme/activities/" + nameOfChosenActivity);
-            c = Class.forName(filePath.toString());
-            System.out.println(c.toString());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
-
         return intent;
     }
 }
