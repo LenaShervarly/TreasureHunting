a7c4fe79d40c512829969d4f8bb42c0255e705c8
Aleksieva89 <vale_aleksieva@yahoo.com>
2017-03-19 22:39:11 +0100

    Valentina's Contribution

--- app/src/main/java/com/home/croaton/followme/activities/GameOverviewActivity.java
+++ app/src/main/java/com/home/croaton/followme/activities/GameOverviewActivity.java
@@ -1,6 +1,7 @@
 package com.home.croaton.followme.activities;
 
 import android.Manifest;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -8,6 +9,10 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
+
 import com.home.croaton.followme.R;
 import com.home.croaton.followme.domain.Game;
 import com.home.croaton.followme.domain.GameFileManager;
@@ -16,9 +21,10 @@ import com.home.croaton.followme.security.PermissionAndConnectionChecker;
 
 public class GameOverviewActivity extends AppCompatActivity  {
 
-    private Game game;
+    private Game excursion;
     private String currentLanguage;
-    private GameFileManager downloadManager;
+    // private FloatingActionButton openButton;
+    private TextView title;
 
 
     @Override
@@ -26,17 +32,39 @@ public class GameOverviewActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_overview);
 
+
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         currentLanguage = sharedPref.getString(getString(R.string.settings_language_preference), "en");
-        game = new Game(this);
+
+        excursion = new Game(this);
+
         PermissionAndConnectionChecker.checkForPermissions(GameOverviewActivity.this, new String[]
                 {Manifest.permission.WRITE_EXTERNAL_STORAGE}, PermissionAndConnectionChecker.LocalStorageRequestCode);
+        bindViews();
+        animateTitle();
+    }
+
+    public void animateTitle() {
+        Animation anim = new ScaleAnimation(
+                1f, 1.5f, // Start and end values for the X axis scaling
+                1f, 1.5f, // Start and end values for the Y axis scaling
+                Animation.RELATIVE_TO_SELF, 1f, // Pivot point of X scaling
+                Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
+        anim.setFillAfter(true); // Needed to keep the result of the animation
+        anim.setDuration(1000);
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setRepeatMode(ValueAnimator.REVERSE);
+        title.startAnimation(anim);
+    }
+
+    public void bindViews(){
+        title = (TextView) findViewById(R.id.title_text_view);
     }
 
     public void onPostExecute(View view) {
         if (ConnectionHelper.hasInternetConnection(GameOverviewActivity.this)) {
             Intent intent = new Intent(GameOverviewActivity.this, MapsActivity.class);
-            intent.putExtra(IntentNames.SELECTED_GAME, game);
+            intent.putExtra(IntentNames.SELECTED_EXCURSION, excursion);
             startActivity(intent);
         }
     }
@@ -54,4 +82,19 @@ public class GameOverviewActivity extends AppCompatActivity  {
         }
     }
 
+
+        /*if (ConnectionHelper.hasInternetConnection(ExcursionOverviewActivity.this)) {
+            MapView mapView = new MapView(ExcursionOverviewActivity.this);
+            MapHelper.chooseBeautifulMapProvider(ExcursionOverviewActivity.this, mapView);
+            mapView.setMinZoomLevel(1);
+            mapView.setMaxZoomLevel(18);
+        }   CacheManager cacheManager = new CacheManager(mapView);
+
+            cacheManager.downloadAreaAsync(ExcursionOverviewActivity.this,
+                    new BoundingBoxE6(excursionBrief.getArea().get(0).getLatitude(),
+                            excursionBrief.getArea().get(1).getLongitude(),
+                            excursionBrief.getArea().get(1).getLatitude(),
+                            excursionBrief.getArea().get(0).getLongitude()), 6, mapView.getMaxZoomLevel());
+
+        }*/
 }
--- app/src/main/java/com/home/croaton/followme/activities/IntentNames.java
+++ app/src/main/java/com/home/croaton/followme/activities/IntentNames.java
@@ -4,6 +4,5 @@ public class IntentNames {
     public static final String SELECTED_GAME_BRIEF = "SELECTED_GAME_BRIEF";
     public static final String SELECTED_GAME_NAME = "SELECTED_GAME_NAME";
     public static final String SELECTED_GAME = "SELECTED_EXCURSION";
-
-
+    public static final String SELECTED_EXCURSION = "SELECTED_EXCURSION";
 }
