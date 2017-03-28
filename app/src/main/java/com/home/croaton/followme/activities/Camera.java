33792db60034429140ade5bd2788429ecb67beae
Temporary User 10231 <tmpuser-10231@kth.se>
2017-03-28 15:34:38 +0200

    Edit main actity and adding camera

---
+++ app/src/main/java/com/home/croaton/followme/activities/Camera.java
@@ -0,0 +1,39 @@
+package com.home.croaton.followme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.home.croaton.followme.R;
+
+public class Camera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void click(View p) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = (ImageView) findViewById(R.id.img);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
