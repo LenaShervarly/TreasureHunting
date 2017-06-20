7b049b048e6b0bd83905d0fc5c306dbb8eb2a3e1
Aleksieva89 <vale_aleksieva@yahoo.com>
2017-06-20 16:55:22 +0200

    Gif animator

---
+++ android/src/com/home/jsquad/knowhunt/android/activities/GIFActivity.java
@@ -0,0 +1,20 @@
+package com.home.jsquad.knowhunt.android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.home.jsquad.knowhunt.R;
+
+public class GIFActivity  extends AppCompatActivity{
+
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
+
+
--- android/src/com/home/jsquad/knowhunt/android/activities/GIFView.java
+++ android/src/com/home/jsquad/knowhunt/android/activities/GIFView.java
@@ -1,81 +1,76 @@
 package com.home.jsquad.knowhunt.android.activities;
 
-/**
- * Created by Valentina 11.04.2017.
- */
-
-import java.io.InputStream;
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Movie;
-import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.View;
 
+
 import com.home.jsquad.knowhunt.R;
+
+import java.io.InputStream;
 
 
 public class GIFView extends View {
 
-        public Movie mMovie;
+    public Movie mMovie;
 
-        public long movieStart;
+    public long movieStart;
 
-        public GIFView(Context context) {
+    public GIFView(Context context) {
 
-            super(context);
+        super(context);
 
-            initializeView(context);
+        initializeView();
 
+    }
+
+    public GIFView(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+
+        initializeView();
+
+    }
+
+    public GIFView(Context context, AttributeSet attrs, int defStyle) {
+
+        super(context, attrs, defStyle);
+        initializeView();
+    }
+
+    private void initializeView() {
+        InputStream is = getContext().getResources().openRawResource(R.drawable.fireworks);
+        mMovie = Movie.decodeStream(is);
+    }
+
+    @Override
+
+    protected void onDraw(Canvas canvas) {
+
+        canvas.drawColor(Color.TRANSPARENT);
+        super.onDraw(canvas);
+        long now = android.os.SystemClock.uptimeMillis();
+        if (movieStart == 0) {
+            movieStart = now;
         }
-
-        public GIFView(Context context, AttributeSet attrs) {
-
-            super(context, attrs);
-
-            initializeView(context);
-
+        if (mMovie != null) {
+            int relTime = (int) ((now - movieStart) % mMovie.duration());
+            mMovie.setTime(relTime);
+            mMovie.draw(canvas, getWidth() - mMovie.width(), getHeight() - mMovie.height());
+            this.invalidate();
         }
+    }
+    private int gifId;
 
-        public GIFView(Context context, AttributeSet attrs, int defStyle) {
+    public void setGIFResource(int resId) {
+        this.gifId = resId;
+        initializeView();
+    }
 
-            super(context, attrs, defStyle);
-            initializeView(context);
-        }
-
-        private void initializeView(Context context) {
-            InputStream is = getContext().getResources().openRawResource(context.getResources().getIdentifier("fireworks", "drawable", context.getPackageName()));
-            mMovie = Movie.decodeStream(is);
-        }
-
-        @Override
-
-        protected void onDraw(Canvas canvas) {
-
-            canvas.drawColor(Color.TRANSPARENT);
-            super.onDraw(canvas);
-            long now = android.os.SystemClock.uptimeMillis();
-            if (movieStart == 0) {
-                movieStart = now;
-            }
-            if (mMovie != null) {
-                int relTime = (int) ((now - movieStart) % mMovie.duration());
-                mMovie.setTime(relTime);
-                mMovie.draw(canvas, getWidth() - mMovie.width(), getHeight() - mMovie.height());
-                this.invalidate();
-            }
-        }
-        private int gifId;
-
-        public void setGIFResource(int resId) {
-            this.gifId = resId;
-            initializeView(getContext());
-        }
-
-        public int getGIFResource() {
-            return this.gifId;
-        }
+    public int getGIFResource() {
+        return this.gifId;
+    }
 }
-
